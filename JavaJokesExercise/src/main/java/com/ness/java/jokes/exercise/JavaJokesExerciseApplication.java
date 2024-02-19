321775f1f0d7830ac6bf6d19f8519ddb1f9c7ed2
P7106133 <P7106133@L7101107976>
2024-02-19 12:40:56 +0530

    Adding files

--- JavaJokesExercise/src/main/java/com/ness/java/jokes/exercise/JavaJokesExerciseApplication.java
+++ JavaJokesExercise/src/main/java/com/ness/java/jokes/exercise/JavaJokesExerciseApplication.java
@@ -1,11 +1,11 @@
-package com.ness.java.jokes.exercise;
+package com.ness.java.jokes.exercise; 
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
 
-@SpringBootApplication
-public class JavaJokesExerciseApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(JavaJokesExerciseApplication.class, args); 
-	}
-}
+@SpringBootApplication 
+public class JavaJokesExerciseApplication { 
+	public static void main(String[] args) { 
+		SpringApplication.run(JavaJokesExerciseApplication.class, args);  
+	} 
+} 
